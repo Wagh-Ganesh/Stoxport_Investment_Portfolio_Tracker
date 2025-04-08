cf7071692821ff26e90e468f5f809beb4642fc17
Ganesh Wagh <ganesh.wagh3003@gmail.com>
2025-04-08 15:30:17 +0530

    first commit

---
+++ src/main/java/com/stoxport/stoxportapp/StoxportappApplication.java
@@ -0,0 +1,13 @@
+package com.stoxport.stoxportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoxportappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoxportappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stoxport/stoxportapp/StoxportappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoxport.stoxportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoxportappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
